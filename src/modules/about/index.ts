2e6affc7dfc83682bb98e645100d9f2bf5f83c4c
melwin <114172287+itzTedx@users.noreply.github.com>
2025-09-03 16:29:45 +0400

    Refactor project structure: rename package to 'maxline-international' and update version to 1.0.2. Remove outdated 'about' and 'contact' feature files to streamline the codebase.

---
+++ src/modules/about/index.ts
@@ -0,0 +1,12 @@
+// Constants
+export * from "./data/constant";
+
+// Components Sections
+export * from './about-us';
+export * from './core-values';
+export * from './hero-banner';
+export * from './mission-vision';
+export * from './overview';
+export * from './team';
+export * from './why-us';
+
